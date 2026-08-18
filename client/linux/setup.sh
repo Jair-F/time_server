2f336169dae2095da4fbac12c8fac013642adfe0
Jair-F <jair.fehlauer@gmail.com>
2026-08-18 22:28:39 +0300

    docker commands

--- client/linux/setup.sh
+++ client/linux/setup.sh
@@ -7,4 +7,5 @@ cp ./chrony.conf /etc/chrony/chrony.conf
 # chronyd -Q 'server 127.0.0.1 maxsamples 1'
 sudo systemctl restart chronyd
 
-
+# docker build -t tmp .
+# docker run -it --rm --privileged --cap-add=SYS_TIME tmp bash
